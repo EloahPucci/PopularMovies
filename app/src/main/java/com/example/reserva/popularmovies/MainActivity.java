1fe50aa313517aee4422bfd59d5868c244504fd5
Aluno <aluno@udicentro.local>
2019-05-20 20:23:17 -0300

    Linha do RecyclerView criada / FilmeAdapter iniciado

--- app/src/main/java/com/example/reserva/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/reserva/popularmovies/MainActivity.java
@@ -2,17 +2,32 @@ package com.example.reserva.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         criandoListaFilmes();
+
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+
+
     }
 
     public void criandoListaFilmes(){
@@ -22,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         filmes.add(new Filme("A menina que roubava livros", 8.9, 8.9, "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.", "2015-02-12", R.drawable.a_menina_que_roubava_livros));
         filmes.add(new Filme("Avatar", 8.9, 8.9, "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.", "2015-02-12", R.drawable.avatar_poster));
         filmes.add(new Filme("Garota exemplar", 8.9, 8.9, "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.", "2015-02-12", R.drawable.garota_exemplar));
-        filmes.add(new Filme("Guardiões da Galáxia I", 8.9, 8.9, "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.", "2015-02-12", R.drawable.guardioes_fa_galaxia));
+        filmes.add(new Filme("Guardiões da Galáxia I", 8.9, 8.9, "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.", "2015-02-12", R.drawable.guardioes_da_galaxia));
         filmes.add(new Filme("Jogos vorazes - Em chamas", 8.9, 8.9, "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.", "2015-02-12", R.drawable.jogos_vorazes));
         filmes.add(new Filme("Malévola", 8.9, 8.9, "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.", "2015-02-12", R.drawable.malevola));
         filmes.add(new Filme("Ninfomaníaca", 8.9, 8.9, "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.", "2015-02-12", R.drawable.ninfomaniaca));

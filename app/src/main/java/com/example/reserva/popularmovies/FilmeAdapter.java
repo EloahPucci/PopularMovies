f92f2563ef0e07e51599bd0f0cb98a58c86732ed
EloahPucci <eloahpucci@gmail.com>
2019-05-20 21:09:00 -0300

    Continuando o Adapter

--- app/src/main/java/com/example/reserva/popularmovies/FilmeAdapter.java
+++ app/src/main/java/com/example/reserva/popularmovies/FilmeAdapter.java
@@ -2,23 +2,49 @@ package com.example.reserva.popularmovies;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class FilmeAdapter extends RecyclerView.Adapter {
+import java.util.List;
+
+public class FilmeAdapter extends RecyclerView.Adapter<FilmeAdapter.ViewHolderFilme> {
+    private List<Filme> listaDeFilmes;
+
+    public FilmeAdapter(List<Filme> listaDeFilmes) {
+        this.listaDeFilmes = listaDeFilmes;
+    }
+
+    public class ViewHolderFilme extends RecyclerView.ViewHolder {
+
+        public ImageView imgFilme;
+
+        public ViewHolderFilme(@NonNull View itemView) {
+            super(itemView);
+
+            imgFilme = (ImageView) itemView.findViewById(R.id.capaGrid);
+        }
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public FilmeAdapter.ViewHolderFilme onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(viewGroup.getContext());
+        View view = layoutInflater.inflate(R.layout.filme_linha, viewGroup, false);
+        ViewHolderFilme viewHolderFilme = new ViewHolderFilme(view);
+
+        return viewHolderFilme;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull FilmeAdapter.ViewHolderFilme viewHolder, int i) {
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listaDeFilmes.size();
     }
 }
